a263751a1b3ae91a6f74c93a7e4f0ed264fe0f83
Lucasn24 <e1406319@u.nus.edu>
2025-08-21 23:08:57 +0800

    rename Duke

---
+++ src/main/java/TalkGPT.java
@@ -0,0 +1,11 @@
+public class TalkGPT {
+    public static void main(String[] args) {
+        String msg = "____________________________________________________________\n"
+                + "Hello! I'm TalkGPT!\n"
+                + "What can I do for you?\n"
+                + "____________________________________________________________\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________";
+        System.out.println(msg);
+    }
+}
