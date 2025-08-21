f04f526ce0d1ab7d9a9d0cdd5cac3ad326588711
Lucasn24 <e1406319@u.nus.edu>
2025-08-22 03:52:38 +0800

    added interactivity

--- src/main/java/TalkGPT.java
+++ src/main/java/TalkGPT.java
@@ -1,11 +1,32 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class TalkGPT {
     public static void main(String[] args) {
-        String msg = "____________________________________________________________\n"
+        String intro = "____________________________________________________________\n"
                 + "Hello! I'm TalkGPT!\n"
                 + "What can I do for you?\n"
-                + "____________________________________________________________\n"
+                + "____________________________________________________________\n";
+        System.out.println(intro);
+
+        Scanner scanner = new Scanner(System.in);
+        //System.out.print("Ask a question?: ");
+        String input = scanner.nextLine();
+
+        while (!Objects.equals(input, "bye")) {
+            String goodbye = "____________________________________________________________\n"
+                    + input + "\n"
+                    + "____________________________________________________________";
+            System.out.println(goodbye);
+
+            //System.out.print("Ask a question?: ");
+            input = scanner.nextLine();
+        }
+
+
+        String goodbye = "____________________________________________________________\n"
                 + "Bye. Hope to see you again soon!\n"
                 + "____________________________________________________________";
-        System.out.println(msg);
+        System.out.println(goodbye);
     }
 }
